4c5163d2439fddcba5b05f359d4b051a92705e85
mufid1012 <mufidghibran1012@gmail.com>
2024-04-23 15:08:30 +0700

    menambahkan modifier public pada prosedut input & output

--- OOP-1.cpp
+++ OOP-1.cpp
@@ -21,5 +21,20 @@ class Matakuliah {
         string kodeMk;
         string namaMk;
         int sks;
+    public :
+        void input() {
+            cout << "Kode MK: ";
+            cin >> kodeMk;
+            cout << "Nama MK: ";
+            cin >> namaMk;
+            cout << "SKS: ";
+            cin >> sks;
+    }
+
+        void output() {
+            cout << "Kode MK: " << kodeMk << endl;
+            cout << "Nama MK: " << namaMk << endl;
+            cout << "SKS: " << sks << endl;
+    }
 
 };
